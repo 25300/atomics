981fafb21411b66869e8bc3fa8292653c3e68aeb
alishavancaspel <25300@ma-web.nl>
2018-09-27 18:23:40 +0200

    update

--- public/includes/home.php
+++ public/includes/home.php
@@ -2,7 +2,5 @@
     <div class="home__header">
         <h1 class="home__title">Atomics</h1>
         <h2 class="home__subtitle">Web development made fun</h2>
-        <h3>hallo</h3>
-        <h4>HALLLOOOOO</h4>
     </div>
 </section>
