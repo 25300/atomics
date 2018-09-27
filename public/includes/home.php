9d924ec8ac5f124056939bd6207ca285ac5429a9
alishavancaspel <25300@ma-web.nl>
2018-09-27 17:39:33 +0200

    added h3

--- public/includes/home.php
+++ public/includes/home.php
@@ -2,5 +2,6 @@
     <div class="home__header">
         <h1 class="home__title">Atomics</h1>
         <h2 class="home__subtitle">Web development made fun</h2>
+        <h3>hallo</h3>
     </div>
 </section>
